1cb6e8a255eeacd7bdbcb5d06e8d0ab497f5792e
cadedi <stscyk@outlook.com>
2021-12-06 18:01:39 +0800

    normal commit

---
+++ single-file-components/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <!-- vetur使用<+v快捷生成vue文件结构 -->
+    <!-- template必须有唯一根元素 -->
+    <school></school>
+    <student></student>
+  </div>
+</template>
+<script>
+  import School from "./School"; //脚手架中vue后缀可不写
+  import Student from "./Student.vue";
+  export default {
+    name: "App",
+    components: {
+      School,
+      Student,
+    },
+  };
+</script>
+
+<style>
+</style>
---
+++ single-file-components/School.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="demo">
+    <h2>学校名称：{{ name }}</h2>
+    <h2>学校地址：{{ address }}</h2>
+    <button @click="showName">点我提示学校名</button>
+  </div>
+</template>
+<script>
+// export default Vue.extend() 简写为配置对象
+export default {
+  name: "School", //指定在Vue开发者工具中的组件名
+  data() {
+    return {
+      name: "少林寺",
+      address: "河南",
+    };
+  },
+  methods: {
+    showName() {
+      alert(this.name);
+    },
+  },
+};
+</script>
+<style>
+.demo {
+  background-color: antiquewhite;
+}
+</style>
